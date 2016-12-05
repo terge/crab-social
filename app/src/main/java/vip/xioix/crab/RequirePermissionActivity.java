d5d45c04af805af0f5b42a9cc7ab15ee7b2fca3c
terge <i.terge.me@gmail.com>
2016-12-05 18:44:51 +0800

    add allo resource

---
+++ app/src/main/java/vip/xioix/crab/RequirePermissionActivity.java
@@ -0,0 +1,22 @@
+package vip.xioix.crab;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class RequirePermissionActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.);
+
+    }
+
+
+}
